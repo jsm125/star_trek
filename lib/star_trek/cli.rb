93646e073f7a03b2e530b3bb3fc7d0873d77a585
Matt Im <matthew.seiim@gmail.com>
2020-06-28 05:28:50 +0000

    add validation check

--- lib/star_trek/cli.rb
+++ lib/star_trek/cli.rb
@@ -1,7 +1,8 @@
 class StarTrek::CLI 
   def greeting
-    puts "Welcome to the Star Trek Non Cinematic Universe!"
-    get_listed_series
+    puts "\nWelcome to the Star Trek Non Cinematic Universe!"
+    get_series
+    list_series
     get_user_series
     # get_seasons_for(series)
     # list_seasons
@@ -10,14 +11,24 @@ class StarTrek::CLI
   end
   
   
-  def get_listed_series
+  def get_series
     @series = ['Star Trek: Discovery', 'Star Trek: The Animated Series', 'Star Trek: The Original Series', 'Star Trek: Enterprise', 'Star Trek: Voyager', 'Star Trek: Deep Space Nine', 'Star Trek: The Next Generation']
   end
 
-  def get_user_series
-    # list series 
+  def list_series
+    puts "\nChoose a series to see the number of seasons.\n"
     @series.each.with_index(1) do |series, index|
       puts "#{index}. #{series}"
     end
+  end
+  
+  def get_user_series
+    chosen_series = gets.strip.to_i
+    # if valid_input(chosen_series, @series)
+    # end
+  end
+
+  def valid_input(input, data)
+    input.to_i <= data.length && input.to_i > 0 
   end
 end
